bbb6270e760a674dfd1f660038fa67793fd89d39
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-05-01 21:05:19 +0530

    Added ServerForm.fxml file

--- src/main/java/lk/ijse/inp/chatapplication/controller/ServerFormController.java
+++ src/main/java/lk/ijse/inp/chatapplication/controller/ServerFormController.java
@@ -1,4 +1,42 @@
 package lk.ijse.inp.chatapplication.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ServerFormController {
+
+    @FXML
+    private Button btnFile;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private AnchorPane serverPane;
+
+    @FXML
+    private TextArea textAreaServer;
+
+    @FXML
+    private TextField txtServer;
+
+    @FXML
+    void btnFileOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSendOnAction(MouseEvent event) {
+
+    }
+
 }
