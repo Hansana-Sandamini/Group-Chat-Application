5bd5fd66d5abc82ac2b79422899602b89471604e
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-05-16 21:48:37 +0530

    Added NameInputForm.fxml file

---
+++ src/main/java/lk/ijse/inp/chatapplication/controller/NameInputFormController.java
@@ -0,0 +1,21 @@
+package lk.ijse.inp.chatapplication.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NameInputFormController {
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnSubmitOnAction(ActionEvent event) {
+
+    }
+
+}
