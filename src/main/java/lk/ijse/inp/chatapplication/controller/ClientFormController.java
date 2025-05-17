16172a6f784f9ea5defdd0b5b7fd95d397bebe57
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-05-17 22:40:52 +0530

    Added ClientForm.fxml file

--- src/main/java/lk/ijse/inp/chatapplication/controller/ClientFormController.java
+++ src/main/java/lk/ijse/inp/chatapplication/controller/ClientFormController.java
@@ -1,4 +1,46 @@
 package lk.ijse.inp.chatapplication.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ClientFormController {
+
+    @FXML
+    private Button btnFile;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    private AnchorPane clientPane;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private TextArea textAreaClient;
+
+    @FXML
+    private TextField txtClient;
+
+    @FXML
+    void btnFileOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSendOnAction(MouseEvent event) {
+
+    }
+
 }
