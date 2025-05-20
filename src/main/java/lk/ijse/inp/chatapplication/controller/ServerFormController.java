cfab6a7c28e56aeaeed5979044310459e5298249
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-05-20 21:24:58 +0530

    show the server messages

--- src/main/java/lk/ijse/inp/chatapplication/controller/ServerFormController.java
+++ src/main/java/lk/ijse/inp/chatapplication/controller/ServerFormController.java
@@ -2,14 +2,25 @@ package lk.ijse.inp.chatapplication.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class ServerFormController {
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerFormController implements Initializable {
 
     @FXML
     private Button btnFile;
@@ -29,6 +40,12 @@ public class ServerFormController {
     @FXML
     private TextField txtServer;
 
+    ServerSocket serverSocket;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String message;
+
     @FXML
     void btnFileOnAction(ActionEvent event) {
 
@@ -36,7 +53,52 @@ public class ServerFormController {
 
     @FXML
     void btnSendOnAction(MouseEvent event) {
-
+        sendServer();
     }
 
+    private void sendServer() {
+        try {
+            String serverMessage = txtServer.getText();
+            dataOutputStream.writeUTF(serverMessage);
+            textAreaServer.appendText("Server : " + serverMessage + "\n");
+            txtServer.clear();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(4000);
+                socket = serverSocket.accept();
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (true) {
+                    message = dataInputStream.readUTF();
+
+                    if (message.equals("IMAGE")) {
+                        int length = dataInputStream.readInt();
+                        byte[] imageBytes = new byte[length];
+                        dataInputStream.readFully(imageBytes);
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);
+                        Image image = new Image(byteArrayInputStream);
+                        imageView.setImage(image);
+                    }
+
+                    if (message.equalsIgnoreCase("exit")) {
+                        break;
+                    }
+                }
+                socket.close();
+                serverSocket.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        textAreaServer.setEditable(false);
+    }
 }
